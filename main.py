59da129d4261025d310d9467c6aaf33bf5ca0d94
daniktertri <tertridanik@gmail.com>
2022-04-23 08:59:07 -0400

    nothing

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import tkinter as tk
+
+root = Tk()
+matrix = [[0 for e in range(20)] for e in range(20)]
+
+def whenPressed(button):
+    button.configure(bg = "yellow")
+
+def near(pos: list , system=[[-1 , -1] , [-1 , 0] , [-1 , 1] , [0 , -1] , [0 , 1] , [1 , -1] , [1 , 0] , [1 , 1]]):
+    count = 0
+    for i in system:
+        if matrix[(pos[0] + i[0]) % len(matrix)][(pos[1] + i[1]) % len(matrix[0])]:
+            count += 1
+    return count
+
+
+for i in range(20):
+    for j in range(20):
+        current_button = Button(root,
+                               text = f" ",
+                               font=("tahoma", 5, "bold"),
+                               height = 1,
+                               width = 1,
+                               bg="gainsboro",
+                               command = lambda: whenPressed(current_button))
+        current_button.grid(row = i+1, column = j+1)
+        matrix[i][j] = current_button
+root.mainloop()
+
+while True:
+    near()
