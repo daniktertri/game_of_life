df1f7cfdc03dd1043d370e0c0bdd32157deeb32a
daniel <tertridaniK@gmail.com>
2022-05-23 11:45:22 -0400

    change bg color

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
-from tkinter import Tk,Canvas
+from tkinter import Tk,Button,Canvas 
+import tkinter
 from model import n,m,matrix,x_cam,y_cam,w_screen,h_screen,width_cam,height_cam
 
 ##tkinter window##
@@ -7,6 +8,8 @@ root.title("Game of life")
 root.geometry(f'{w_screen}x{h_screen}')
 
 ##init canvas##
-canvas = Canvas(root, bg = "white")
+canvas = Canvas(root, bg = "black")
 canvas.pack(fill="both",expand=True)
+
+##init button##
 
