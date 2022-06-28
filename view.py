c900dc1e7ca7d18b121fead7c0cb391caddb43a5
daniktertri <80840120+daniktertri@users.noreply.github.com>
2022-06-28 15:24:31 -0400

    Update view.py

--- view.py
+++ view.py
@@ -2,11 +2,11 @@ from tkinter import Tk,Button,Canvas
 import tkinter
 from model import n,m,matrix,x_cam,y_cam,w_screen,h_screen,width_cam,height_cam
 
-##tkinter window##
+##tkinter##
 root = Tk()
 root.title("Game of life")
 root.geometry(f'{w_screen}x{h_screen}')
 
-##init canvas##
+##canvas##
 canvas = Canvas(root, bg = "black")
 canvas.pack(fill="both",expand=True)
