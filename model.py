2d051044f1183246400940ad5e24910368c6ed2f
daniktertri <80840120+daniktertri@users.noreply.github.com>
2022-06-28 15:23:57 -0400

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import random
 n,m = 50,50
 matrix = [[False for e in range(m)] for e in range(n)]
 
-##data##
+##main data##
 x_cam, y_cam = 0.3,0.7
 width_cam, height_cam = 10,10
 w_screen,h_screen = 806, 636
